d1ca6d5053f543db78702fcbd2d23eb2942d863b
Liza Kaladjian <95773312+Lizoug@users.noreply.github.com>
2023-03-06 15:38:14 +0100

    Update App.tsx
    added all plosts

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -11,7 +11,63 @@ export default function App() {
                 "correct": [[]],
                 "incorrect": [[]]
             },
+           "petal length vs. petal width": {
+                "correct": [[]],
+                "incorrect": [[]]
+            },
             "petal length vs. sepal length": {
+                "correct": [[]],
+                "incorrect": [[]]
+            },
+            "petal length vs. sepal width": {
+                "correct": [[]],
+                "incorrect": [[]]
+            },
+            "petal width vs. petal length": {
+                "correct": [[]],
+                "incorrect": [[]]
+            },
+            "petal width vs. petal width": {
+                "correct": [[]],
+                "incorrect": [[]]
+            },
+            "petal width vs. sepal length": {
+                "correct": [[]],
+                "incorrect": [[]]
+            },
+            "petal width vs. sepal width": {
+                "correct": [[]],
+                "incorrect": [[]]
+            },
+            "sepal length vs. petal length": {
+                "correct": [[]],
+                "incorrect": [[]]
+            },
+            "sepal length vs. petal width": {
+                "correct": [[]],
+                "incorrect": [[]]
+            },
+            "sepal length vs. sepal length": {
+                "correct": [[]],
+                "incorrect": [[]]
+            },
+            "sepal length vs. sepal width": {
+                "correct": [[]],
+                "incorrect": [[]]
+            },
+            "sepal width vs. petal length": {
+                "correct": [[]],
+                "incorrect": [[]]
+            },
+            "sepal width vs. petal width": {
+                "correct": [[]],
+                "incorrect": [[]]
+            },
+            "sepal width vs. sepal length": {
+                "correct": [[]],
+                "incorrect": [[]]
+            },
+            "sepal width vs. sepal width": {
                 "correct": [[]],
                 "incorrect": [[]]
             }
