64dd6ab7e3286c4c380ad5ff510d2c2b2906bf1e
Liza Kaladjian <95773312+Lizoug@users.noreply.github.com>
2023-04-20 23:05:05 +0200

    new endpoint
    added a new API endpoint to predict the iris-art of the input from the user

--- Backend/apis/iris_api.py
+++ Backend/apis/iris_api.py
@@ -1,5 +1,8 @@
-from fastapi import (APIRouter, Path, Query)
+from fastapi import (APIRouter, Path, Query, Body)
 from toolbox.iris_functions import ABC, create_dataset, eval_knn, iris_data
+from pydantic import BaseModel
+from joblib import load
+from sklearn.datasets import load_iris
 
 router = APIRouter()
 
@@ -22,3 +25,35 @@ async def datapoints():
     data = iris_data(X_test, pred_eval)
 
     return data
+
+
+class FlowerInput(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+@router.post("/predict")
+async def predict(input_data: FlowerInput = Body(...)):
+    sepal_length = input_data.sepal_length
+    sepal_width = input_data.sepal_width
+    petal_length = input_data.petal_length
+    petal_width = input_data.petal_width
+
+    # Load the saved model
+    model = load("ml_models/knn_iris.joblib")
+
+    # Load the iris dataset
+    iris_data = load_iris()
+
+    # Predict the iris flower type
+    X_new = [[sepal_length, sepal_width, petal_length, petal_width]]
+    prediction = model.predict(X_new)
+
+    # Ensure that the prediction is an integer
+    predicted_index = int(prediction[0])
+
+    flower_type = iris_data.target_names[predicted_index]
+
+    # Return the predicted flower type as a JSON response
+    return {"flower_type": flower_type}
