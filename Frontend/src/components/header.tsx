56541db1cc35239bfb503dbb8e3c58267c1fd0ef
Liza Kaladjian <95773312+Lizoug@users.noreply.github.com>
2023-06-05 15:29:51 +0200

    Update header.tsx

--- Frontend/src/components/header.tsx
+++ Frontend/src/components/header.tsx
@@ -1,41 +1,7 @@
-import { Menu, Row, Col, Divider } from "antd";
-import React, { useState } from 'react';
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { Menu, Row, Col } from "antd";
+import React, { useState} from 'react';
+import { HomeOutlined, DotChartOutlined, UserOutlined } from '@ant-design/icons';
 import type { MenuProps } from 'antd';
-
-
-
-export function PageHeader2() {
-    return (
-        <div className="header">
-            <Row align="middle">
-                <Col span={12}>
-                    <div className="header__logo">
-                        <a href="#">
-                            <img src="https://countryroadsmagazine.com/downloads/12278/download/isi-web.jpg?cb=09a504f17c004fecc03f38f2797b02c3&w=1050&h=" alt="Header Image" />
-                        </a>
-                    </div>
-                </Col>
-                <Col span={12}>
-                    <nav className="header__nav">
-                        <ul className="header__nav-list">
-                            <li className="header__nav-item">
-                                <a href="/" className="header__nav-link">Home</a>
-                            </li>
-                            <li className="header__nav-item">
-                                <a href="/About-Us" className="header__nav-link">About Us</a>
-                            </li>
-                            <li className="header__nav-item">
-                                <a href="/Analysis" className="header__nav-link">Analysis</a>
-                            </li>
-                        </ul>
-                    </nav>
-                </Col>
-            </Row>
-        </div>
-    );
-}
-
 
 
 const items: MenuProps['items'] = [
@@ -44,22 +10,21 @@ const items: MenuProps['items'] = [
             <a href="/">
                 Home
             </a>,
-
-        key: 'mail',
-        icon: <MailOutlined />,
+        key: '/',
+        icon: <HomeOutlined />,
     },
     {
         label:
             <a href="/About-us">
                 About us
             </a>,
-        key: 'app',
-        icon: <AppstoreOutlined />,
+        key: '/About-us',
+        icon: <UserOutlined />,
     },
     {
         label: 'Analysis',
         key: 'SubMenu',
-        icon: <SettingOutlined />,
+        icon: <DotChartOutlined />,
         children: [
             {
                 type: 'group',
@@ -67,38 +32,49 @@ const items: MenuProps['items'] = [
                 children: [
                     {
                         label:
-                            <a href="Analysis/Iris-flower">
-                                Iris flower
+                            <a href="/Analysis/Iris-flower">
+                                Overview
                             </a>,
-                        key: 'setting:1',
+                        key: '/Iris-flower',
                     }
                 ],
             },
+
+            {
+                type: 'group',
+                label: 'Item 2',
+                children: [
+                    {
+                        label:
+                            <a href="/Analysis/FlowerPredictor">
+                                Flower prediction
+                            </a>,
+                        key: '/interactive',
+                    }
+                ],
+            },
+
         ],
     }
 ];
 
 export function  PageHeader() {
 
-    const [current, setCurrent] = useState('mail');
+    const [current, setCurrent] = useState(window.location.pathname);
 
     const onClick: MenuProps['onClick'] = (e) => {
         console.log('click ', e);
         setCurrent(e.key);
     };
 
+
     return (
-        <div>
+        <div className="header">
             <Row>
-                <Col>
-                    <h1 className={"title"}>
-                        Iris Flower
-                    </h1>
-                </Col>
-                <Col>
+                <Col className="header-tabs" span={24}>
                     <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
                 </Col>
             </Row>
         </div>
-    )
+            )
 };
