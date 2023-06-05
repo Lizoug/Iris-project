fd0106d7747883af75b7e130b045a878151d6608
Liza Kaladjian <95773312+Lizoug@users.noreply.github.com>
2023-06-05 15:30:20 +0200

    Update router.tsx

--- Frontend/src/components/router.tsx
+++ Frontend/src/components/router.tsx
@@ -1,6 +1,8 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import {Home} from "./pages/home";
 import Iris from "./pages/iris";
+import {AboutUs} from "./pages/about-us";
+import {FlowerPredictor} from "./pages/FlowerPredictor";
 
 export default function Routing() {
     return (
@@ -9,7 +11,8 @@ export default function Routing() {
                 <Routes>
                     <Route path="/" element={<Home/>}/>
                     <Route path="/Analysis/Iris-flower" element={<Iris/>} />
-                    <Route path="/About-us" element={<Iris/>}/>
+                    <Route path="/Analysis/FlowerPredictor" element={<FlowerPredictor/>}/>
+                    <Route path="/About-us" element={<AboutUs/>}/>
                 </Routes>
             </BrowserRouter>
         </div>
