c4112e6f27bac275569cdad7aa698fa1516cc1d4
Liza Kaladjian <95773312+Lizoug@users.noreply.github.com>
2023-06-05 15:46:42 +0200

    Update iris_models.py

--- Backend/models/iris_models.py
+++ Backend/models/iris_models.py
@@ -14,3 +14,10 @@ class IrisCategories(BaseModel):
     data: dict[str, IrisDataPoints]
 
 
+class FlowerInput(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+
